2abc27fc4142ca4f44620c40f132cec6c8fb2746
Joeri Klomp <joeri.klomp@vrt.be>
2023-09-15 21:44:44 +0200

    added oh-my-zsh install

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ apt install -y curl
 apt install -y zsh
 
 #--oh-my-zsh--
-sh -c -y "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#--after installation, switch to zsh--
+zsh
