797dc8b633f1dec9d7deedc50d90b330721a5fbe
Joeri Klomp <joeri.klomp@vrt.be>
2023-09-15 22:56:18 +0200

    added oh-my-zsh install

--- install.sh
+++ install.sh
@@ -13,4 +13,5 @@ yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/mast
 zsh
 
 #--get a custom setup of oh-my-zsh--
-(echo "n"; echo "n"; echo "n"; echo "y"; echo "1"; echo "2"; echo "2"; echo "3"; echo "1"; echo "3"; echo "2"; echo "1"; echo "n"; echo "1"; echo "y") | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+(echo "n"; echo "n"; echo "n"; echo "y"; echo "1"; echo "2"; echo "2"; echo "3"; echo "1"; echo "3"; echo "2"; echo "1"; echo "n"; echo "1"; echo "y") | 
